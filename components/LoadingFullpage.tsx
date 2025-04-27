1e93986b8bc590ce378fb41edb9a86e4e84e59c3
Hoang Nguyen V. <hoang.nguyen@classmethod.tech>
2025-04-27 14:46:22 +0700

    Remove commented content

--- components/LoadingFullpage.tsx
+++ components/LoadingFullpage.tsx
@@ -30,38 +30,6 @@ const LoadingFullPage = () => {
       })}
     </div>
   );
-
-  // const fallingTomato = [
-  //   {
-  //     id: 1,
-  //     className: "first-tomato",
-  //   },
-  //   {
-  //     id: 2,
-  //     className: "second-tomato",
-  //   },
-  //   {
-  //     id: 3,
-  //     className: "third-tomato",
-  //   },
-  //   {
-  //     id: 4,
-  //     className: "forth-tomato",
-  //   },
-  // ];
-
-  // return (
-  //   <div className="opacity-bg fixed inset-0 grid grid-cols-12 bg-white overflow-hidden z-9999">
-  //     {fallingTomato.map((item) => {
-  //       return (
-  //         <div
-  //           key={item.id}
-  //           className={`${item.className}  bg-[var(--muted)] col-span-3 flex justify-center`}
-  //         ></div>
-  //       );
-  //     })}
-  //   </div>
-  // );
 };
 
 export default LoadingFullPage;
